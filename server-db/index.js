f7a3977333c189d7ae2df577a0d5b53e0fcaefdb
Ian <ian.loschinskey@gmmail.com>
2023-10-02 01:34:18 -0400

    deleted error cpnt bcrypt

--- server-db/index.js
+++ server-db/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bcrypt = require('bcrypt');
 const mongoose = require("mongoose");
 
 require("dotenv").config();
