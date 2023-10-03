18269f5736067ce680da54db15ade1f3cf562966
Ian <ian.loschinskey@gmmail.com>
2023-10-02 21:46:10 -0400

    figuring deploy problem.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Log In button', () => {
+  const { container } = render(<App />);
+  console.log(container.innerHTML);
+  const loginButton = screen.getByText('Log In', { exact: false, ignoreCase: true });
+  expect(loginButton).toBeInTheDocument();
 });
