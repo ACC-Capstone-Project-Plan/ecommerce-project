fec3656abd122106d7f435133a92418e8f4fa713
Ian <ian.loschinskey@gmmail.com>
2023-10-08 18:54:41 -0400

    hotfix

--- server-db/connectMongo.js
+++ server-db/connectMongo.js
@@ -1,1 +1,12 @@
-z
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+            console.log("Connect to MongoDB successfully")
+    } catch (error) {
+        console.log("Connect failed")
+    }
+}
+
+module.exports = connectDB
