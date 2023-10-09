7a757b677766a81c85f28c99956523d401e521a0
Ian <ian.loschinskey@gmmail.com>
2023-10-09 19:46:59 -0400

    login fix3

--- server-db/index.js
+++ server-db/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -11,7 +12,7 @@ const connectDB = require("./connectMongo");
 connectDB();
 
 const products = require("./models/products");
-const allUser = require("./models/users");
+const allUser = require("./models/users")
 
 // Use the 'cors' middleware to enable CORS
 app.use(cors());
@@ -100,197 +101,107 @@ app.delete("/products/:id", async (req, res) => {
 
 // All Users
 app.get("/users", async (req, res) => {
-  try {
-    return res.status(200).json({
-      msg: "OK",
-      data: allUser,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
-// Register a new user
-app.post("/register", async (req, res) => {
-  try {
-    const { username, password, name, email, phone, address } = req.body;
-
-    // Check if the username is already taken
-    const existingUser = allUser.find((user) => user.username === username);
-    if (existingUser) {
-      return res.status(400).json({
-        msg: "Username already exists",
+    try {
+      return res.status(200).json({
+        msg: "OK",
+        data: allUser,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        msg: error.message,
       });
     }
+  });
 
-    // Generate a salt for password hashing (you can configure the number of rounds)
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-
-    // Hash the password
-    const hashedPassword = bcrypt.hashSync(password, salt);
-
-    // Create a new user with the hashed password and additional fields
-    const newUser = {
-      username,
-      password: hashedPassword,
-      name,
-      email,
-      phone,
-      address,
-    };
-
-    // Add the new user to your user data array
-    allUser.push(newUser);
-
-    return res.status(200).json({
-      msg: "User registered successfully",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
-// Update user information, including password
-app.put("/users/:userId", async (req, res) => {
-  try {
-    const userId = parseInt(req.params.userId);
-    const { username, password, name, email, phone, address } = req.body;
-
-    // Find the user by userId
-    const userIndex = allUser.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      return res.status(404).json({
-        msg: "User not found",
-      });
-    }
-
-    // Update user data
-    const updatedUser = allUser[userIndex];
-
-    if (username) {
-      // Check if the new username is already taken
-      const existingUser = allUser.find(
-        (user) => user.username === username && user.id !== userId
-      );
+  app.post("/register", async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Check if the username is already taken
+      const existingUser = allUser.find(user => user.username === username);
       if (existingUser) {
         return res.status(400).json({
           msg: "Username already exists",
         });
       }
-      updatedUser.username = username;
-    }
-
-    if (password) {
-      // Generate a salt for password hashing (you can configure the number of rounds)
-      const saltRounds = 10;
-      const salt = bcrypt.genSaltSync(saltRounds);
-
-      // Hash the new password
-      const hashedPassword = bcrypt.hashSync(password, salt);
-      updatedUser.password = hashedPassword;
-    }
-
-    if (name) updatedUser.name = name;
-    if (email) updatedUser.email = email;
-    if (phone) updatedUser.phone = phone;
-    if (address) updatedUser.address = address;
-
-    // Update the user data in the array
-    allUser[userIndex] = updatedUser;
-
-    return res.status(200).json({
-      msg: "User information updated successfully",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
-
-// Delete user account
-app.delete("/users/:userId", async (req, res) => {
-  try {
-    const userId = parseInt(req.params.userId);
-
-    // Find the user by userId
-    const userIndex = allUser.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      return res.status(404).json({
-        msg: "User not found",
+  
+      // Hash the password before saving it to the database
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create a new user object
+      const newUser = {
+        username,
+        password: hashedPassword,
+      };
+  
+      // Add the new user to the allUser array (or save it to your database)
+      allUser.push(newUser);
+  
+      return res.status(200).json({
+        msg: "User registered successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        msg: error.message,
       });
     }
-
-    // Remove the user from the user data array
-    allUser.splice(userIndex, 1);
-
-    return res.status(200).json({
-      msg: "User account deleted successfully",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
+  });
+  
 
 // Login
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
+  
+    // Check if username and password are present
+    if (!username || !password) {
+      return res.status(400).json({ msg: "Username and password are required" });
+    }
+  
+    // Find a user with the matching username and password
+    const matchedUser = allUser.find(
+      (user) =>
+        user.username.trim() === username.trim() &&
+        user.password.trim() === password.trim()
+    );
+  
+    if (matchedUser) {
+      // If a user is found, send back the userId
+      const userId = matchedUser.id;
+      res.status(200).json({ userId });
+    } else {
+      // If no matching user is found, send an error response
+      res.status(401).json({ msg: "Invalid username or password" });
+    }
+  });
+  
 
-  // Check if username and password are present
-  if (!username || !password) {
-    return res.status(400).json({ msg: "Username and password are required" });
-  }
-
-  // Find a user with the matching username and password
-  const matchedUser = allUser.find(
-    (user) =>
-      user.username.trim() === username.trim() &&
-      user.password.trim() === password.trim()
-  );
-
-  if (matchedUser) {
-    // If a user is found, send back the userId
-    const userId = matchedUser.id;
-    res.status(200).json({ userId });
-  } else {
-    // If no matching user is found, send an error response
-    res.status(401).json({ msg: "Invalid username or password" });
-  }
-});
-
-
-app.get("/user/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
-    // Convert userId to an integer
-    const id = parseInt(userId);
-
-    // Find the user by ID in the allUser array
-    const user = allUser.find((u) => u.id === id);
-
-    if (!user) {
-      return res.status(404).json({
-        msg: "User not found",
+  app.get("/user/:id", async (req, res) => {
+    try {
+      const userId = req.params.id;
+      // Convert userId to an integer
+      const id = parseInt(userId);
+  
+      // Find the user by ID in the allUser array
+      const user = allUser.find((u) => u.id === id);
+  
+      if (!user) {
+        return res.status(404).json({
+          msg: "User not found",
+        });
+      }
+  
+      return res.status(200).json({
+        msg: "OK",
+        data: user,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        msg: error.message,
       });
     }
-
-    return res.status(200).json({
-      msg: "OK",
-      data: user,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: error.message,
-    });
-  }
-});
+  });
+  
+  
 
 const PORT = process.env.PORT || 5000;
 
